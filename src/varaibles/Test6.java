25a0ba36f6f0fec65908be2af4ba2ca7691bc013
QSP <QSP@DESKTOP-EVK9GMC>
2020-11-26 17:47:19 +0530

    Methods

--- src/varaibles/Test6.java
+++ src/varaibles/Test6.java
@@ -2,8 +2,11 @@ package varaibles;
 
 public class Test6 {
 
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String dob="5-10-2330";
+		String dob1="28/03/1998";
+		System.out.println(dob);
 
 	}
 
