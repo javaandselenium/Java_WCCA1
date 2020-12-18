2866eec322b5cb522849d8f3e21a5bc632642110
QSP <QSP@DESKTOP-EVK9GMC>
2020-12-18 17:53:53 +0530

    Interface

---
+++ src/accessSpecifiers/Test5.java
@@ -0,0 +1,35 @@
+package accessSpecifiers;
+
+import com.sun.tools.javac.launcher.Main;
+
+public class Test5 {
+
+//	public static void main(String[] args) {
+//		Demo d=new Demo();
+//		d.demo();
+//		Demo1 d1=new Demo1();
+//		d1.demo1();
+	public void demo()
+	{
+
+	}
+
+}
+
+class Demo{
+	public void demo() {
+		System.out.println("jgdzshgh");
+		
+	}
+	public static void main(String[] args) {
+		Demo d=new Demo();
+		d.demo();
+	}
+}
+
+class Demo1{
+	public void demo1() {
+		System.out.println("jgdzshgh");
+		
+	}
+}
